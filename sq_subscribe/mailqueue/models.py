6ec12d3c920c94cc1baa4478d039dc93c5e294bd
Ivan Varnavsky <astron.x-ray@mail.ru>
2013-08-20 09:05:42 +0600

    v0.5.4

--- sq_subscribe/mailqueue/models.py
+++ sq_subscribe/mailqueue/models.py
@@ -9,6 +9,9 @@ from django.core.mail.message import EmailMultiAlternatives, EmailMessage
 from django.db import models
 from django.template.loader import render_to_string, get_template_from_string
 from django.utils.html import strip_tags
+from django.conf import settings
+from apps.main.utils import ATTACHMENT_PATH
+import os
 
 
 CONTENT_TYPE = [
@@ -25,6 +28,7 @@ class MailQueue(models.Model):
     created_date = models.DateTimeField(default=datetime.now())
     content_type = models.CharField(max_length=100, blank=True, choices=CONTENT_TYPE)
 
+
     def __unicode__(self):
         return u'Mail message %s' % self.subject
 
@@ -36,7 +40,6 @@ class MailQueue(models.Model):
 
     def send_email(self,connecion):
         vars = json.loads(self.message)
-        from django.conf import settings
         template_directory =  getattr(settings, "EMAIL_TEMPLATE_DIR", 'email')
         if self.template.endswith('.html') or self.template.endswith('.txt'):
             try:
@@ -66,26 +69,40 @@ class MailQueue(models.Model):
                     msg = EmailMessage(self.subject,text_content,from_email=self.send_from,to=[send_to_list[0]],connection=connecion, bcc=send_to_list[1:], headers={'Cc': ','.join(send_to_list[1:])})
                 else:
                     msg = EmailMessage(self.subject,text_content,from_email=self.send_from,to=[send_to_list[0]],connection=connecion)
+            if ('attachment' in vars['data']) and (vars['data']['attachment']!={}):
+                path = vars['data']['attachment']['att_file_path']
+                with open(path, 'r') as f:
+                    msg.attach(vars['data']['attachment']['att_file_name'], f.read(), vars['data']['attachment']['att_file_type'])
+                f.closed
+                os.remove(path)
         except Exception:
             raise Exception('Email message %s can not created.'%self.id)
         self.delete()
         return msg
 
-def create_mailqueue(subject, template, send_to, content_type, message=None, send_from=None):
+def create_mailqueue(subject, template, send_to, content_type, message=None, send_from=None, attachment=None):
     if not message: message = {}
-    from django.conf import settings
+    if not attachment: attachment = {}
     if send_from is None:
         send_from = settings.DEFAULT_FROM_EMAIL
     site = Site.objects.get_current()
-    message.update({"site":{'sitename': site.name,'domain':site.domain}})
+    message.update({"site":{'sitename': site.name,'domain':site.domain},"attachment":attachment})
     msg = {"data":message}
     mail = MailQueue.objects.create(message=json.dumps(msg),send_to=send_to,subject=subject,template=template,send_from=send_from,content_type=content_type)
     mail.save()
     return mail
 
 
-def send_email(subject,template,send_to,content_type,message=None,send_from=None):
+def send_email(subject,template,send_to,content_type,message=None,send_from=None,attachment=None):
+    attach_t = {}
+    if attachment is not None:
+        att_file_path = ATTACHMENT_PATH + u'/' + attachment['att_file_name']
+        with open(att_file_path, 'w') as f:
+            f.write(attachment['att_file'])
+            f.closed
+            attach_t.update({'att_file_name': attachment['att_file_name'], 'att_file_path': att_file_path, 'att_file_type': attachment['att_file_type']})
+
     from sq_subscribe.mailqueue.tasks import send_concrete_mailqueue
-    mail = create_mailqueue(subject,template,send_to,content_type,message,send_from)
+    mail = create_mailqueue(subject,template,send_to,content_type,message,send_from,attach_t)
     #TODO нужно придумать, как сделать проверку - отправлять ли письмо по таску или мгновенно.
     send_concrete_mailqueue.delay([mail.id])
